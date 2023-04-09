c1f76e06e425948be6d37683f78562ea2ba284d1
DmitriiObushko <105298559+DmitriiObushko@users.noreply.github.com>
2023-04-09 15:14:03 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,34 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-
+#include <string>
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private:
+    T arr[100];
+    int highest;
+public:
+    TStack() : highest(-1) {}
+    void push(T value) {
+        if (!isFull())
+            arr[++highest] = value;
+        else
+            throw std::string("Full");
+    }
+    T get() const {
+        return arr[highest];
+    }
+    T pop() {
+        if (isEmpty())
+            throw std::string("Empty");
+        else
+            return arr[highest--];
+    }
+    bool isEmpty() const {
+        return highest == -1;
+    }
+    bool isFull() const {
+        return highest == size - 1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
